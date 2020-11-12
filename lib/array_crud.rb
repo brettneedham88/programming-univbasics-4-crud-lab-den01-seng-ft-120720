5d33a46dee3124257d6cee6061fbc24a70ff7fcc
Brett Needham <brettneedham88@gmail.com>
2020-11-12 17:43:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,8 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  afc_west
-  [broncos, raiders, chiefs, chargers]
+  afc_west = [broncos, raiders, chiefs, chargers]
 end
 
 def add_element_to_end_of_array(array, element)
