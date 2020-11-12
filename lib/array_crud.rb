3daaaa96a4e45f7abaafff16798d56878a47cf90
Brett Needham <brettneedham88@gmail.com>
2020-11-12 18:05:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  afc_west << "steelers"
+  element = "49ers"
+  nfc = ["49ers"]
 end
 
 def add_element_to_start_of_array(array, element)
