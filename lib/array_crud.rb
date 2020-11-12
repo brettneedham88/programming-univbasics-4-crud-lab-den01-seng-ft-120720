db3fa68892113c6ef363dcbe5abfbbbe4bf9b92d
Brett Needham <brettneedham88@gmail.com>
2020-11-12 18:13:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,8 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["I", "am", "really", "learning"]
-  
+  element = "wow"
+  array.shift("wow")
 end
 
 def remove_element_from_end_of_array(array)
