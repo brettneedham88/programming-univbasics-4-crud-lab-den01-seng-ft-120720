a0478e72e0a0ef701323094db3aaa942c4115b5c
Brett Needham <brettneedham88@gmail.com>
2020-11-12 18:19:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  last_word = 
+  last_word = array
 end
 
 def remove_element_from_start_of_array(array)
